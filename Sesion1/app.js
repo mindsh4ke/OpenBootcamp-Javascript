e69f41aafe8e17655ccefc0bfd32f5f64b65b3c1
mindsh4ke <105060443+mindsh4ke@users.noreply.github.com>
2022-07-08 17:26:26 +0200

    Sesión 1

---
+++ Sesion1/app.js
@@ -0,0 +1,10 @@
+let nombre = "Alba"
+let edad = 19
+let eresDesarrollador = true
+let fechaNacimiento = new Date('2003', '06', '24')
+let libroFavorito = {
+    titulo: "Libro de Ejemplo",
+    autor: "Autor de Ejemplo",
+    fecha: new Date(),
+    url: "www.google.com"
+}
